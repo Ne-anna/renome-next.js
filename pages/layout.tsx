5cbcc7090e4f769164b60cd3070caaa4d53f7ba9
Ne-anna <annabusele@inbox.lv>
2022-07-15 16:35:41 +0300

    Create layout with dummy data

---
+++ pages/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout({ children }: { children: any }) {
+  return (
+    <div>
+      <p>Navigation coming soon</p>
+      {children}
+      <p>Footer coming soon</p>
+    </div>
+  );
+}
