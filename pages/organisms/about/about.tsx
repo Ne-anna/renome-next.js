8f4a62b8d58af158f016dd671418b4db9c6ef999
Ne-anna <annabusele@inbox.lv>
2022-07-15 16:34:38 +0300

    Based on atomic design folder structure,path chang

--- pages/organisms/about/about.tsx
+++ pages/organisms/about/about.tsx
@@ -1,4 +1,4 @@
-import style from "../styles/about.module.scss";
+import style from "../../../styles/about.module.scss";
 import { AboutData } from "../../../data";
 
 export default function About(props: AboutData) {
