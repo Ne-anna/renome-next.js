bbe395a61f6b73ce108f604ab11aff6b1d8f6e3d
Ne-anna <annabusele@inbox.lv>
2022-07-15 11:52:35 +0300

    Add fetched data

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,9 +4,9 @@ import { AboutData } from "../data";
 export default function About(props: AboutData) {
   return (
     <div className={style.aboutContainer}>
-      <h1 className={style.aboutTitle}></h1>
-      <h5 className={style.aboutSubTitle}></h5>
-      <p className={style.aboutParagraph}></p>
+      <h1 className={style.aboutTitle}>{props.title}</h1>
+      <h5 className={style.aboutSubTitle}>{props.subTitle}</h5>
+      <p className={style.aboutParagraph}>{props.text}</p>
     </div>
   );
 }
