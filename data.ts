1f9062be876dc452f49e287c6ae4c121f138489a
Ne-anna <annabusele@inbox.lv>
2022-07-15 11:51:27 +0300

    Add data.ts which contains whole project interface

---
+++ data.ts
@@ -0,0 +1,168 @@
+export interface Cart {
+  cart: CartItem[];
+}
+
+export interface CartItem {
+  item: string;
+}
+
+export interface NavigationData {
+  logo: string;
+  cartPath: string;
+  altTagCart: string;
+  count: string;
+  dividerPath: string;
+  altTagDivider: string;
+}
+
+export interface MenuData {
+  menu: MenuItem[];
+  openSubMenu?(): void;
+  closeSubMenu?(): void;
+}
+
+export interface MenuItem {
+  title?: string;
+  buttonSearchPath?: string;
+  searchAltTag?: string;
+  url?: string;
+  buttonNextPath?: string;
+  buttonNextAltTag?: string;
+  isSubMenu?: boolean;
+  subMenu?: SubMenu[];
+}
+
+export interface SubMenu {
+  title: string;
+  url: string;
+}
+
+export interface backButtonData {
+  buttonData: BackButtonTitle;
+}
+
+export interface BackButtonTitle {
+  title?: string;
+  buttonBackPath: string;
+  buttonBackAltTag: string;
+}
+
+export interface CarouselData {
+  carousel: CarouselItem[];
+  slideIndex?: number;
+  slideDirection?: string;
+  prevSlide?: number;
+  blockAnimation?: boolean;
+}
+
+export interface CarouselItem {
+  title: string;
+  subTitle: string;
+  altTag: string;
+  path: string;
+}
+
+export interface CarouselButton {
+  carouselButtonNext?: string;
+  altTagNext?: string;
+  carouselButtonPrevious?: string;
+  altTagPrevious?: string;
+  loadPreviousSlide?(): void;
+  loadNextSlide?(): void;
+}
+
+export interface Images {
+  top: string;
+  altTagBack: string;
+  bottom: string;
+  altTagFront: string;
+}
+
+export interface AboutData {
+  images: Images;
+  title: string;
+  subTitle: string;
+  text: string;
+}
+
+export interface GalleryImage {
+  altTag: string;
+  path: string;
+}
+
+export interface GalleryData {
+  title: string;
+  subTitle: string;
+  galleryImages: GalleryImage[];
+}
+
+export interface FooterIcon {
+  id: number;
+  icon: string;
+  altTag: string;
+}
+
+export interface FooterData {
+  title: string;
+  footerIcons: FooterIcon[];
+  logo: string;
+  copyright: string;
+}
+export interface RootObject {
+  cart: CartItem[];
+  navigation: NavigationData;
+  menu: MenuItem[];
+  backButtonTitle: BackButtonTitle;
+  carousel: CarouselItem[];
+  carouselButtons: CarouselButton;
+  about: AboutData;
+  gallery: GalleryData;
+  footer: FooterData;
+}
+
+export const defaultData: RootObject = {
+  cart: [],
+  navigation: {
+    logo: "",
+    cartPath: "",
+    altTagCart: "",
+    count: "",
+    dividerPath: "",
+    altTagDivider: "",
+  },
+  menu: [],
+  backButtonTitle: {
+    title: "",
+    buttonBackPath: "",
+    buttonBackAltTag: "",
+  },
+  carousel: [],
+  carouselButtons: {
+    carouselButtonNext: "",
+    altTagNext: "",
+    carouselButtonPrevious: "",
+    altTagPrevious: "",
+  },
+  about: {
+    images: {
+      top: "",
+      altTagBack: "",
+      bottom: "",
+      altTagFront: "",
+    },
+    title: "",
+    subTitle: "",
+    text: "",
+  },
+  gallery: {
+    title: "",
+    subTitle: "",
+    galleryImages: [],
+  },
+  footer: {
+    title: "",
+    footerIcons: [],
+    logo: "",
+    copyright: "",
+  },
+};
