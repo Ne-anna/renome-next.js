dea997bc63df57969f4aeb82dc42ad7aec5e5d53
Ne-anna <annabusele@inbox.lv>
2022-07-19 16:43:24 +0300

    Remove style from tsx file

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,12 +23,6 @@ export const getStaticProps: GetStaticProps = async () => {
 export default function RenomeApp({ allData }: allDataProps) {
   return (
     <>
-      <style>{`
-        body {
-          margin: 0px;
-          padding: 0px;
-        }
-      `}</style>
       <Head>
         <title>Renome next.js</title>
       </Head>
