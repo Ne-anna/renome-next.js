78de10dd2d207b3f01344780346785fc80095eba
Ne-anna <annabusele@inbox.lv>
2022-07-20 16:15:21 +0300

    Create navigation.tsx

---
+++ pages/organisms/navigation/navigation.tsx
@@ -0,0 +1,37 @@
+import { NavigationData } from "../../../data";
+import style from "../navigation/navigation.module.scss";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Navigation(props: NavigationData) {
+  const [isMainMenuOpen, setMainMenuOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setMainMenuOpen(!isMainMenuOpen);
+  };
+
+  return (
+    <div className={style.nav}>
+      <h2 className={style.navigationLeft}>{props.logo}</h2>
+      <div className={style.navigationRight}>
+        <div className={style.navigationCart}>
+          <Image
+            src={props.cartPath}
+            alt={props.altTagCart}
+            width={18}
+            height={25}
+          />
+        </div>
+        <p className={style.navigationCount}>{props.count}</p>
+        <div className={style.navigationDivide}>
+          <Image
+            src={props.dividerPath}
+            alt={props.altTagDivider}
+            width={20}
+            height={25}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
