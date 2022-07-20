6224cb3c792f4a5b757a3e2fc4f0a09af5af2903
Ne-anna <annabusele@inbox.lv>
2022-07-20 10:55:17 +0300

    Create module.scss,implement in footer.tsx+props

---
+++ pages/organisms/footer/footer.tsx
@@ -0,0 +1,29 @@
+import { FooterData } from "../../../data";
+import FooterIcon from "../../molecules/footer-icon/footer-icon";
+import style from "../footer/footer.module.scss";
+
+export default function Footer(props: FooterData) {
+  return (
+    <div className={style.footer}>
+      <h5 className={style.footerTitle}>{props.title}</h5>
+      <div className={style.footerSocialMedia}>
+        <div className={style.footerIcon}>
+          {props.footerIcons.map((items, index) => {
+            return (
+              <div key={index}>
+                <FooterIcon
+                  icon={items.icon}
+                  altTag={items.altTag}
+                  id={items.id}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <h2 className={style.footerLogo}>{props.logo}</h2>
+      <h5 className={props.copyright}>{props.copyright}</h5>
+    </div>
+  );
+}
