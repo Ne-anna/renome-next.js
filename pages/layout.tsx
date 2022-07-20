dcd411040e4ba7929628730d640b02aea3e2b314
Ne-anna <annabusele@inbox.lv>
2022-07-20 10:54:05 +0300

    Create interface, pass data to Footer component

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,9 +1,22 @@
-export default function Layout({ children }: { children: any }) {
+import Footer from "./organisms/footer/footer";
+import { FooterData } from "../data";
+
+interface FooterDataProps {
+  footerDataProps: FooterData;
+  children: React.ReactNode;
+}
+
+export default function Layout(props: FooterDataProps) {
   return (
     <div>
       <p>Navigation coming soon</p>
-      {children}
-      <p>Footer coming soon</p>
+      {props.children}
+      <Footer
+        title={props.footerDataProps.title}
+        logo={props.footerDataProps.logo}
+        copyright={props.footerDataProps.copyright}
+        footerIcons={props.footerDataProps.footerIcons}
+      />
     </div>
   );
 }
