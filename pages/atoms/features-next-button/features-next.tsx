1568e5c6aec87443c986167293a9f63af02d5c05
Ne-anna <annabusele@inbox.lv>
2022-07-21 16:47:48 +0300

    Create "next" button for features section in menu

---
+++ pages/atoms/features-next-button/features-next.tsx
@@ -0,0 +1,18 @@
+import style from "../features-next-button/features-next.module.scss";
+import Image from "next/image";
+import { MenuItem } from "../../../data";
+
+export default function FeaturesNext(props: MenuItem) {
+  return (
+    <button className={style.button}>
+      {props.buttonNextPath && (
+        <Image
+          src={props.buttonNextPath}
+          alt={props.buttonNextAltTag}
+          width={15}
+          height={15}
+        />
+      )}
+    </button>
+  );
+}
