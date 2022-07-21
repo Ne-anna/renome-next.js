5d7103a98d504bbdc2255a152e8f5c7e802845de
Ne-anna <annabusele@inbox.lv>
2022-07-21 16:48:30 +0300

    Solve <Image/> missing src error

--- pages/atoms/search-button/search-button.tsx
+++ pages/atoms/search-button/search-button.tsx
@@ -2,15 +2,17 @@ import { MenuItem } from "../../../data";
 import Image from "next/image";
 import style from "../search-button/search-button.module.scss";
 
-export default function Search({ buttonSearchPath, searchAltTag }: MenuItem) {
+export default function Search(props: MenuItem) {
   return (
     <button className={style.button}>
-      <Image
-        src={buttonSearchPath}
-        alt={searchAltTag}
-        width={200}
-        height={200}
-      />
+      {props.buttonSearchPath && (
+        <Image
+          src={props.buttonSearchPath}
+          alt={props.searchAltTag}
+          width={20}
+          height={20}
+        />
+      )}
     </button>
   );
 }
