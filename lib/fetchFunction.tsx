afe8ab3fcaf52fb45c709644cc6b8367650780b9
Ne-anna <annabusele@inbox.lv>
2022-07-21 15:59:38 +0300

    Change source

--- lib/fetchFunction.tsx
+++ lib/fetchFunction.tsx
@@ -1,6 +1,6 @@
 export async function getSortedPostsData() {
   const requestURL =
-    "https://raw.githubusercontent.com/Ne-anna/Renome-react/main/src/db.json";
+    "https://raw.githubusercontent.com/Ne-anna/renome-typescript/main/src/db.json";
   const request = await fetch(requestURL);
 
   return await request.json();
