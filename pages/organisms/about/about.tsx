9b13ec03750eec79485905e3225c65848e6c6747
Ne-anna <annabusele@inbox.lv>
2022-08-03 14:44:25 +0300

    Implement Grid component, create interface

--- pages/organisms/about/about.tsx
+++ pages/organisms/about/about.tsx
@@ -1,12 +1,25 @@
 import style from "../about/about.module.scss";
-import { AboutData } from "../../../data";
+import Grid from "../../molecules/grid-images/grid";
+import { AboutData, Images } from "../../../data";
+interface AboutDataProps {
+  textData: AboutData;
+  imageData: Images;
+}
 
-export default function About(props: AboutData) {
+export default function About(props: AboutDataProps) {
   return (
     <div className={style.aboutContainer}>
-      <h1 className={style.aboutTitle}>{props.title}</h1>
-      <h5 className={style.aboutSubTitle}>{props.subTitle}</h5>
-      <p className={style.aboutParagraph}>{props.text}</p>
+      <Grid
+        altTagBack={props.imageData.altTagBack}
+        top={props.imageData.top}
+        bottom={props.imageData.bottom}
+        altTagFront={props.imageData.altTagFront}
+      />
+      <div className={style.about}>
+        <h1 className={style.aboutTitle}>{props.textData.title}</h1>
+        <h5 className={style.aboutSubTitle}>{props.textData.subTitle}</h5>
+        <p className={style.aboutParagraph}>{props.textData.text}</p>
+      </div>
     </div>
   );
 }
