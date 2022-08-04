4407459985cf53cbbaf539eb28fc125019af74ee
Ne-anna <annabusele@inbox.lv>
2022-08-04 17:44:01 +0300

    Error part2

--- pages/organisms/about/about.test.tsx
+++ pages/organisms/about/about.test.tsx
@@ -1,9 +1,19 @@
-import { render, screen, cleanup } from "@testing-library/react";
-import About from "./about";
+import { render, screen } from "@testing-library/react";
+import About, { AboutDataProps } from "./about";
+
+interface NeedData {
+  allData: AboutDataProps;
+}
+
+// let props: AboutDataProps;
 
 it("should render content props correctly", () => {
-  render(<About textData.title
-    ="WE CREATE DELICOUS MEMORIES" />);
+  render(
+    <About
+      textData={props.allData.textData}
+      imageData={props.allData.imageData}
+    />
+  );
   // expect(screen.getByRole("h5", { name: "WE CREATE DELICOUS MEMORIES" }));
-  expect(screen.getByText("WE CREATE DELICOUS MEMORIES")).toBeInTheDocument();
+  expect(screen.getByText(props.allData.textData.title)).toBeInTheDocument();
 });
