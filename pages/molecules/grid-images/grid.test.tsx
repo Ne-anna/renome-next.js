9dc7e8fc78f7a0668bb4f7c31d283d7b4e50ea2e
Ne-anna <annabusele@inbox.lv>
2022-08-08 15:17:46 +0300

    Add data-testid and create test file

---
+++ pages/molecules/grid-images/grid.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import Grid from "./grid";
+import "@testing-library/jest-dom";
+
+const imageData = {
+  top: "",
+  altTagBack: "white house from afar",
+  bottom: "",
+  altTagFront: "close-up second house floor window",
+};
+
+it("time to check Grid components back images", () => {
+  render(
+    <Grid
+      top={imageData.top}
+      bottom={imageData.bottom}
+      altTagBack={imageData.altTagBack}
+      altTagFront={imageData.altTagFront}
+    />
+  );
+  const backImage = screen.getByTestId("backImage");
+  expect(backImage).toBeInTheDocument();
+});
+
+it("time to check Grid components front images", () => {
+  render(
+    <Grid
+      top={imageData.top}
+      bottom={imageData.bottom}
+      altTagBack={imageData.altTagBack}
+      altTagFront={imageData.altTagFront}
+    />
+  );
+  const frontImage = screen.getByTestId("frontImage");
+  expect(frontImage).toBeInTheDocument();
+});
