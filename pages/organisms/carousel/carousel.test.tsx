e45fec75813fba3133f8639c5380aa98dd6ecb93
Ne-anna <annabusele@inbox.lv>
2022-08-11 15:56:33 +0300

    getByRole, rename, remove commented out parts

--- pages/organisms/carousel/carousel.test.tsx
+++ pages/organisms/carousel/carousel.test.tsx
@@ -1,15 +1,9 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { CarouselButton, CarouselData, CarouselItem } from "../../../data";
 import Carousel from "./carousel";
 import CarouselPreviousButton from "../../atoms/carousel-previous-button/carousel-previous-button";
 import CarouselNextButton from "../../atoms/carousel-next-button/carousel-next-button";
-
-// enum SlideDirections {
-//   current = "current",
-//   right = "right",
-//   left = "left",
-// }
 
 const carouselItem: CarouselItem = {
   title: "Made with love",
@@ -25,10 +19,6 @@ interface CarouselProps {
 
 const carouselData: CarouselData = {
   carousel: [carouselItem],
-  // slideIndex: 2,
-  // slideDirection: SlideDirections.current,
-  // prevSlide: 1,
-  // blockAnimation: true,
 };
 
 const carouselButtonData: CarouselButton = {
@@ -62,53 +52,35 @@ it("Let's check carousel title, sub-title and image altTag", () => {
 });
 
 //better to test buttons from parent compoent then create seperate test file in child component
-it("Is 'previous' button clicked?", () => {
-  const handleClickMock = jest.fn();
-  const { getByTestId } = render(
-    <CarouselPreviousButton loadPreviousSlide={handleClickMock} />
-  );
-  const button = screen.getByTestId("buttonPrevious");
-  fireEvent.click(button);
-  expect(handleClickMock).toHaveBeenCalledTimes(1);
-});
-
-it("Is 'previous' button clicked?", () => {
-  const handleClickMock = jest.fn();
-  const { getByTestId } = render(
-    <CarouselNextButton loadNextSlide={handleClickMock} />
-  );
-  const button = screen.getByTestId("buttonNext");
-  fireEvent.click(button);
-  expect(handleClickMock).toHaveBeenCalledTimes(1);
-});
 
 it("Test 'next' button", () => {
-  const { getByTestId } = render(<CarouselNextButton />);
-  expect(getByTestId("buttonNext")).toBeInTheDocument();
+  const { getByRole } = render(<CarouselNextButton />);
+  const button = screen.getByRole("button", { name: "next button" });
+  expect(button).toBeInTheDocument();
 });
 
 it("Test 'previous' button", () => {
-  const { getByTestId } = render(<CarouselPreviousButton />);
-  expect(getByTestId("buttonPrevious")).toBeInTheDocument();
+  const { getByRole } = render(<CarouselPreviousButton />);
+  const button = screen.getByRole("button", { name: "previous button" });
+  expect(button).toBeInTheDocument();
 });
 
-// it("Is 'next' button clicked?", () => {
-//   const handleClickMock = jest.fn();
-//   const { getByTestId } = render(
-//     <CarouselNextButton loadNextSlide={handleClickMock} />
-//   );
-//   const button = screen.getByTestId("buttonTest2");
-//   fireEvent.click(button);
-//   expect(handleClickMock).toHaveBeenCalledTimes(1);
-// });
+it("Is 'previous' button clicked?", () => {
+  const clickPrevious = jest.fn();
+  const { getByRole } = render(
+    <CarouselPreviousButton loadPreviousSlide={clickPrevious} />
+  );
+  const button = screen.getByRole("button", { name: "previous button" });
+  fireEvent.click(button);
+  expect(clickPrevious).toHaveBeenCalledTimes(1);
+});
 
-// it("Let's check carousel button image altTag", () => {
-//   render(
-//     <Carousel
-//       carouselData={carouselProps.carouselData}
-//       carouselButtonData={carouselProps.carouselButtonData}
-//     />
-//   );
-//   const buttonAltTag = screen.getByAltText(carouselButtonData.altTagNext);
-//   expect(buttonAltTag).toBeInTheDocument();
-// });
+it("Is 'previous' button clicked?", () => {
+  const clickNext = jest.fn();
+  const { getByRole } = render(
+    <CarouselNextButton loadNextSlide={clickNext} />
+  );
+  const button = screen.getByRole("button", { name: "next button" });
+  fireEvent.click(button);
+  expect(clickNext).toHaveBeenCalledTimes(1);
+});
