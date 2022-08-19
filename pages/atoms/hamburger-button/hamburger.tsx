27f4ddba2a5f09a704615a0a522773383188d33c
Ne-anna <annabusele@inbox.lv>
2022-08-19 15:38:09 +0300

    Add role and aria-label to hamburger

--- pages/atoms/hamburger-button/hamburger.tsx
+++ pages/atoms/hamburger-button/hamburger.tsx
@@ -2,7 +2,7 @@ import style from "../../organisms/navigation/navigation.module.scss";
 
 const Hamburger = () => {
   return (
-    <div>
+    <div role="hamburger" aria-label="hamburger button">
       <span className={style.navigationHamburgerBar}></span>
       <span className={style.navigationHamburgerBar}></span>
       <span className={style.navigationHamburgerBar}></span>
