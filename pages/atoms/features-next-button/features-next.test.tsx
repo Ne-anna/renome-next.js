649edef0b6b7f5193e7cf75d6ac6d4318a09e296
Ne-anna <annabusele@inbox.lv>
2022-08-19 12:16:29 +0300

    Add async and await

--- pages/atoms/features-next-button/features-next.test.tsx
+++ pages/atoms/features-next-button/features-next.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import FeaturesNext from "./features-next";
 
-it("Render features next button", () => {
+it("Render features next button", async () => {
   render(<FeaturesNext />);
-  const nextButton = screen.findByRole("button", {
+  const nextButton = await screen.findByRole("button", {
     name: "features next button",
   });
   expect(nextButton).toBeInTheDocument();
