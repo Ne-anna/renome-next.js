d51d9baa60e63e892ee95660d6a2605e1517d8c2
Ne-anna <annabusele@inbox.lv>
2022-08-24 11:25:45 +0300

    Remove unnecessary lines of code

--- pages/atoms/carousel-next-button/carousel-next.test.tsx
+++ pages/atoms/carousel-next-button/carousel-next.test.tsx
@@ -3,16 +3,14 @@ import "@testing-library/jest-dom";
 import CarouselNextButton from "./carousel-next-button";
 
 it("Render 'next' button", () => {
-  const { getByRole } = render(<CarouselNextButton />);
+  render(<CarouselNextButton />);
   const button = screen.getByRole("button", { name: "next button" });
   expect(button).toBeInTheDocument();
 });
 
 it("Is 'next' button clicked?", () => {
   const clickNext = jest.fn();
-  const { getByRole } = render(
-    <CarouselNextButton loadNextSlide={clickNext} />
-  );
+  render(<CarouselNextButton loadNextSlide={clickNext} />);
   const button = screen.getByRole("button", { name: "next button" });
   fireEvent.click(button);
   expect(clickNext).toHaveBeenCalledTimes(1);
--- pages/atoms/carousel-previous-button/carousel-previous.test.tsx
+++ pages/atoms/carousel-previous-button/carousel-previous.test.tsx
@@ -3,16 +3,14 @@ import "@testing-library/jest-dom";
 import CarouselPreviousButton from "./carousel-previous-button";
 
 it("Render'previous' button", () => {
-  const { getByRole } = render(<CarouselPreviousButton />);
+  render(<CarouselPreviousButton />);
   const button = screen.getByRole("button", { name: "previous button" });
   expect(button).toBeInTheDocument();
 });
 
 it("Is 'previous' button clicked?", () => {
   const clickPrevious = jest.fn();
-  const { getByRole } = render(
-    <CarouselPreviousButton loadPreviousSlide={clickPrevious} />
-  );
+  render(<CarouselPreviousButton loadPreviousSlide={clickPrevious} />);
   const button = screen.getByRole("button", { name: "previous button" });
   fireEvent.click(button);
   expect(clickPrevious).toHaveBeenCalledTimes(1);
